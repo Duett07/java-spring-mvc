1a433d187fc350fda26116fbccdbf4588d9a088a
Lo Van Du <dulo200720061984@gmail.com>
2025-01-06 23:48:11 +0700

    update new

---
+++ src/main/java/vn/du/laptopshop/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package vn.du.laptopshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.du.laptopshop.domain.User;
+import java.util.List;
+
+//crud: create, read, update, delete
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User du);
+
+    void deleteById(long id);
+
+    List<User> findByEmail(String email);
+
+    List<User> findAll();
+
+    User findById(long id);
+
+}
--- src/main/java/vn/du/laptopshop/service/UserService.java
+++ src/main/java/vn/du/laptopshop/service/UserService.java
@@ -1,11 +1,38 @@
 package vn.du.laptopshop.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import vn.du.laptopshop.domain.User;
+import vn.du.laptopshop.repository.UserRepository;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
-    public String handleHello() {
-        return "Hello from service";
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        User du = this.userRepository.save(user);
+        return du;
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void deleteAUser(long id) {
+        this.userRepository.deleteById(id);
     }
 }
